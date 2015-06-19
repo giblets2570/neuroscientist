3e7a6bce95effe0d2d3966d5ccbfbd4909fa8faf
giblets2570 <tomkeohanemurray@gmail.com>
2015-06-19 09:49:17 +0100

    removed my weights and let lasagne do it, learns better to begin

--- feed-forward/net.py
+++ feed-forward/net.py
@@ -37,6 +37,8 @@ BATCH_SIZE = 600
 NUM_HIDDEN_UNITS = 100
 LEARNING_RATE = 0.01
 MOMENTUM = 0.9
+EARLY_STOPPING = True
+STOPPING_RANGE = 10
 
 def floatX(X):
     """
@@ -69,7 +71,7 @@ def load_data():
         output_dim=y_train.shape[1],
     )
 
-def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, w_h1, w_h2, w_h3, w_h4, w_o,batch_size=BATCH_SIZE):
+def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden,batch_size=BATCH_SIZE):
 	"""Create a symbolic representation of a neural network with `intput_dim`
     input nodes, `output_dim` output nodes and `num_hidden_units` per hidden
     layer.
@@ -86,7 +88,6 @@ def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, 
             l_in,
             num_units=num_hidden_units,
             nonlinearity=lasagne.nonlinearities.rectify,
-            W=w_h1
             )
 
         # l_hidden_dropout = lasagne.layers.DropoutLayer(
@@ -97,7 +98,6 @@ def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, 
             l_hidden,
             num_units=num_hidden_units,
             nonlinearity=lasagne.nonlinearities.rectify,
-            W=w_h2
             )
         # l_hidden_2_dropout = lasagne.layers.DropoutLayer(
         #     l_hidden,
@@ -107,7 +107,6 @@ def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, 
         #     l_hidden_2,
         #     num_units=num_hidden_units,
         #     nonlinearity=lasagne.nonlinearities.rectify,
-        #     W=w_h3
         #     )
         # # l_hidden_3_dropout = lasagne.layers.DropoutLayer(
         # #     l_hidden_3,
@@ -117,7 +116,6 @@ def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, 
         #     l_hidden_3,
         #     num_units=num_hidden_units,
         #     nonlinearity=lasagne.nonlinearities.rectify,
-        #     W=w_h4
         #     )
         # l_hidden_4_dropout = lasagne.layers.DropoutLayer(
         #     l_hidden_4,
@@ -127,32 +125,29 @@ def model(input_dim, output_dim, num_hidden_units, p_drop_input, p_drop_hidden, 
             l_hidden_2,
             num_units=output_dim,
             nonlinearity=lasagne.nonlinearities.softmax,
-            W=w_o
             )
         return l_out
 
-def funcs(dataset, noiseyNetworkLayer, cleanNetworkLayer, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, momentum=MOMENTUM):
+def funcs(dataset, network, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, momentum=MOMENTUM):
 
     # symbolic variables 
     X_batch = T.matrix()
     y_batch = T.matrix()
 
     # this is the cost of the network when fed throught the noisey network
-    train_output = lasagne.layers.get_output(noiseyNetworkLayer, X_batch)
+    train_output = lasagne.layers.get_output(network, X_batch)
     cost = lasagne.objectives.categorical_crossentropy(train_output, y_batch)
     cost = cost.mean()
 
     # test the performance of the netowork without noise
-    test = lasagne.layers.get_output(cleanNetworkLayer, X_batch, deterministic=True)
+    test = lasagne.layers.get_output(network, X_batch, deterministic=True)
     pred = T.argmax(test, axis=1)
     accuracy = T.mean(T.eq(pred, y_batch), dtype=theano.config.floatX)
 
-    all_params = lasagne.layers.get_all_params(noiseyNetworkLayer)
-    updates = lasagne.updates.nesterov_momentum(
-        cost, all_params, learning_rate, momentum)
+    all_params = lasagne.layers.get_all_params(network)
+    updates = lasagne.updates.nesterov_momentum(cost, all_params, learning_rate, momentum)
     
     train = theano.function(inputs=[X_batch, y_batch], outputs=cost, updates=updates, allow_input_downcast=True)
-    # predict = theano.function(inputs=[X_batch,y_batch], outputs=accuracy, allow_input_downcast=True)
     predict = theano.function(inputs=[X_batch], outputs=pred, allow_input_downcast=True)
 
     return dict(
@@ -163,24 +158,30 @@ def funcs(dataset, noiseyNetworkLayer, cleanNetworkLayer, batch_size=BATCH_SIZE,
 def main():
     dataset = load_data()
 
-    w_h1 = init_weights((dataset['input_dim'], NUM_HIDDEN_UNITS))
-    w_h2 = init_weights((NUM_HIDDEN_UNITS, NUM_HIDDEN_UNITS))
-    w_h3 = init_weights((NUM_HIDDEN_UNITS, NUM_HIDDEN_UNITS))
-    w_h4 = init_weights((NUM_HIDDEN_UNITS, NUM_HIDDEN_UNITS))
-    w_o = init_weights((NUM_HIDDEN_UNITS, dataset['output_dim']))
-
     print("Making networks with shared weights")
-    noiseyNetwork = model(dataset['input_dim'],dataset['output_dim'],NUM_HIDDEN_UNITS,0.2,0.2,w_h1,w_h2,w_h3,w_h4,w_o)
-    cleanNetwork = model(dataset['input_dim'],dataset['output_dim'],NUM_HIDDEN_UNITS,0.,0.,w_h1,w_h2,w_h3,w_h4,w_o)
+    network = model(dataset['input_dim'],dataset['output_dim'],NUM_HIDDEN_UNITS,0.2,0.2)
+    # cleanNetwork = model(dataset['input_dim'],dataset['output_dim'],NUM_HIDDEN_UNITS,0.,0.,w_h1,w_h2,w_h3,w_h4,w_o)
     print("Done")
 
-    training = funcs(dataset,noiseyNetwork,cleanNetwork)
+    training = funcs(dataset,network)
+
+    accuracies = []
 
     for i in range(NUM_EPOCHS):
         for start, end in zip(range(0, dataset['num_examples_train'], BATCH_SIZE), range(BATCH_SIZE, dataset['num_examples_train'], BATCH_SIZE)):
             cost = training['train'](dataset['X_train'][start:end],dataset['y_train'][start:end])
-        print(training['predict'](dataset['X_test']).shape)
-        print(np.mean(np.argmax(dataset['y_test'], axis=1) == training['predict'](dataset['X_test'])))
+        accuracy = np.mean(np.argmax(dataset['y_test'], axis=1) == training['predict'](dataset['X_test']))
+        print(accuracy)
+        if(EARLY_STOPPING):
+            if(len(accuracies) < STOPPING_RANGE):
+                accuracies.append(accuracy)
+            else:
+                test = [k for k in accuracies if k < accuracy]
+                if not test:
+                    print('Early stopping causing training to finish at epoch {}'.format(k))
+                    break
+                del accuracies[0]
+                accuracies.append(accuracy)
 
 
 if __name__ == '__main__':
