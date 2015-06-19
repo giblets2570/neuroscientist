9b896bd829b342496ebee42b6224d8920dbf496e
giblets2570 <tomkeohanemurray@gmail.com>
2015-06-19 10:48:05 +0100

    added logging of experiments

--- feed-forward/net.py
+++ feed-forward/net.py
@@ -14,7 +14,10 @@ import lasagne
 import theano
 import theano.tensor as T
 import time
+import datetime
 from data import formatData
+import json
+import re
 
 PY2 = sys.version_info[0] == 2
 
@@ -32,7 +35,7 @@ else:
 
 BASENAME = "../../R2192/20140110_R2192_track1"
 
-NUM_EPOCHS = 20
+NUM_EPOCHS = 10
 BATCH_SIZE = 600
 NUM_HIDDEN_UNITS = 100
 LEARNING_RATE = 0.01
@@ -40,6 +43,10 @@ MOMENTUM = 0.9
 
 EARLY_STOPPING = True
 STOPPING_RANGE = 10
+
+LOG_EXPERIMENT = True
+
+TETRODE_NUMBER = 16
 
 def load_data(tetrode_number):
     """
@@ -144,9 +151,9 @@ def funcs(dataset, network, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, 
         predict=predict
     )
 
-def main():
+def main(tetrode_number=TETRODE_NUMBER):
     print("Loading the data...")
-    dataset = load_data(16)
+    dataset = load_data(tetrode_number)
     print("Done!")
 
     print("Making the model...")
@@ -159,11 +166,13 @@ def main():
 
     accuracies = []
 
+
+    print("Begining to train the network...")
     for i in range(NUM_EPOCHS):
         for start, end in zip(range(0, dataset['num_examples_train'], BATCH_SIZE), range(BATCH_SIZE, dataset['num_examples_train'], BATCH_SIZE)):
             cost = training['train'](dataset['X_train'][start:end],dataset['y_train'][start:end])
         accuracy = np.mean(np.argmax(dataset['y_test'], axis=1) == training['predict'](dataset['X_test']))
-        print(accuracy)
+        print("Epoch: {}, Accuracy: {}".format(i+1,accuracy))
 
         if(EARLY_STOPPING):
             if(len(accuracies) < STOPPING_RANGE):
@@ -171,11 +180,33 @@ def main():
             else:
                 test = [k for k in accuracies if k < accuracy]
                 if not test:
-                    print('Early stopping causing training to finish at epoch {}'.format(k))
+                    print('Early stopping causing training to finish at epoch {}'.format(i))
                     break
                 del accuracies[0]
                 accuracies.append(accuracy)
 
+    if(LOG_EXPERIMENT):
+        print("Logging the experiment details...")
+        log = dict(
+            TETRODE_NUMBER = TETRODE_NUMBER,
+            BASENAME = BASENAME,
+            NUM_EPOCHS = NUM_EPOCHS,
+            BATCH_SIZE = BATCH_SIZE,
+            NUM_HIDDEN_UNITS = NUM_HIDDEN_UNITS,
+            LEARNING_RATE = LEARNING_RATE,
+            MOMENTUM = MOMENTUM,
+            ACCURACY = accuracies[-1],
+            NETWORK_LAYERS = [str(type(layer)) for layer in lasagne.layers.get_all_layers(network)],
+            # NETWORK_PARAMS = lasagne.layers.get_all_params_values(network)
+        )
+        now = datetime.datetime.now()
+        filename = "experiments/{}_{}_{}_NUMLAYERS_{}".format(now,NUM_EPOCHS,NUM_HIDDEN_UNITS,len(log['NETWORK_LAYERS']))
+        filename = re.sub("[^A-Za-z0-9_/ ,-:]", "", filename)
+        with open(filename,"w") as outfile:
+            json.dump(log, outfile)
+
+
 
 if __name__ == '__main__':
-    main()
+    for i in range(16):
+        main(i+1)
