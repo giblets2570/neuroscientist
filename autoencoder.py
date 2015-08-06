87af9166845f722c1889f4bfcb2a8e4c4ff7418e
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-06 16:40:00 +0100

    sdfadsf

--- autoencoder.py
+++ autoencoder.py
@@ -179,7 +179,7 @@ def model(input_shape, output_dim, num_hidden_units,num_hidden_units_2, num_code
 
         return l_out
 
-def funcs(dataset, network, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, sparsity=0.02, beta=0.1, momentum=MOMENTUM):
+def funcs(dataset, network, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, sparsity=0.02, beta=0.01, momentum=MOMENTUM):
 
     """
         Method the returns the theano functions that are used in 
