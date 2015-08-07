18f268413a669943993225826a978795a6d89fdb
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-07 11:05:00 +0100

    fdsafsd

--- code_layer_video.py
+++ code_layer_video.py
@@ -239,7 +239,7 @@ def main(tetrode_number=TETRODE_NUMBER,num_hidden_units=300,num_hidden_units_2=2
     print("Done!")
 
 
-    for tetrode_number in [9]:#,10,11,12,13,14,15,16]:
+    for tetrode_number in [9,10,11,12,13,14,15,16]:
 
         print("Loading the model parameters from {}".format(MODEL_FILENAME+str(tetrode_number)))
         f = open(MODEL_FILENAME+str(tetrode_number),'r')
