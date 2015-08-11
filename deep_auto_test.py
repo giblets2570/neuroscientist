6e0377ba32f2c2a4f7f9f5cabfd58b81dea66223
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-11 09:58:54 +0100

    plotting the cost per sequence

--- deep_auto_test.py
+++ deep_auto_test.py
@@ -253,7 +253,7 @@ def main(tetrode_number=TETRODE_NUMBER,num_hidden_units=500,num_hidden_units_2=3
     print("Done!")
 
 
-    for tetrode_number in [9]:
+    for tetrode_number in [12]:
 
         print("Loading the model parameters from {}".format(MODEL_FILENAME+str(tetrode_number)))
         f = open(MODEL_FILENAME+str(tetrode_number),'r')
@@ -299,46 +299,48 @@ def main(tetrode_number=TETRODE_NUMBER,num_hidden_units=500,num_hidden_units_2=3
         # codes = training['code'](dataset['data'][0:NUM_POINTS])
         # # print(codes.shape)
         # # codes_2d = bh_sne(codes)
-        
-        codes_2d = bh_sne(10*np.asarray(codes,dtype=np.float64))
 
-        # m = TSNE(n_components=2, random_state=0)
-        
-        # codes_2d = m.fit_transform(codes[:NUM_POINTS])
-        # activations_1_2d = bh_sne(activations_1)
-        # activations_2_2d = bh_sne(activations_2)
+        for k in range(3):
+            print(k)
+            codes_2d = bh_sne(np.asarray(codes[:(k+1)*12000],dtype=np.float64))
 
-        plt.scatter(codes_2d[:, 0], codes_2d[:, 1], c=dataset['labels'][0:NUM_POINTS],alpha=0.8,lw=0)
-        plt.savefig('dbscan_labels/deep/sparse/tsne_codes_{}.png'.format(tetrode_number), bbox_inches='tight')
-        plt.close()
+            # m = TSNE(n_components=2, random_state=0)
+            
+            # codes_2d = m.fit_transform(codes[:NUM_POINTS])
+            # activations_1_2d = bh_sne(activations_1)
+            # activations_2_2d = bh_sne(activations_2)
 
-        # This is where the code for the video will go
-        ##############################################################################
-        # Compute DBSCAN
-        db = None
-        core_samples_mask = None
-        labels = None
+            plt.scatter(codes_2d[:, 0], codes_2d[:, 1], c=dataset['labels'][0:NUM_POINTS],alpha=0.8,lw=0)
+            plt.savefig('dbscan_labels/deep/sparse/tsne_codes_{}_{}.png'.format(tetrode_number,k), bbox_inches='tight')
+            plt.close()
 
-        num_labels = 0
-        eps=1.0
-        while(num_labels < 10):
-            db = DBSCAN(eps=eps, min_samples=10).fit(codes_2d)
-            core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-            core_samples_mask[db.core_sample_indices_] = True
-            labels = db.labels_
-            num_labels = np.amax(labels)
-            eps -= 0.1
+            # This is where the code for the video will go
+            ##############################################################################
+            # Compute DBSCAN
+            db = None
+            core_samples_mask = None
+            labels = None
 
-        print("Num learned labels: {}".format(num_labels))
+            num_labels = 0
+            eps=0.8
+            while(num_labels < 10):
+                db = DBSCAN(eps=eps, min_samples=10).fit(codes_2d)
+                core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+                core_samples_mask[db.core_sample_indices_] = True
+                labels = db.labels_
+                num_labels = np.amax(labels)
+                eps -= 0.1
 
-        plt.title('Estimated number of clusters: {}'.format(np.amax(labels)))
-        plt.scatter(codes_2d[:, 0], codes_2d[:, 1], c=labels[0:NUM_POINTS],lw=0)
-        plt.savefig('dbscan_labels/deep/sparse/dbscan_codes_{}.png'.format(tetrode_number), bbox_inches='tight')
-        plt.close()
+            print("Num learned labels: {}".format(num_labels))
 
-        f=open('dbscan_labels/deep/sparse/tetrode_{}.npy'.format(tetrode_number),'w')
-        pickle.dump(labels, f)
-        f.close()
+            plt.title('Estimated number of clusters: {}'.format(np.amax(labels)))
+            plt.scatter(codes_2d[:, 0], codes_2d[:, 1], c=labels[0:NUM_POINTS],lw=0)
+            plt.savefig('dbscan_labels/deep/sparse/dbscan_codes_{}_{}.png'.format(tetrode_number,k), bbox_inches='tight')
+            plt.close()
+
+            # f=open('dbscan_labels/deep/sparse/tetrode_{}.npy'.format(tetrode_number),'w')
+            # pickle.dump(labels, f)
+            # f.close()
 
 if __name__ == '__main__':
     main()
