4b6f5c1cea88478c51010cd0b0b65c3253f3b446
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-21 13:40:23 +0100

    yay

--- recurrent_test_argmax.py
+++ recurrent_test_argmax.py
@@ -358,7 +358,7 @@ def main(tetrode_number=TETRODE_NUMBER):
             break
 
     print(cost_arrays)
-    points_from = 0
+    points_from = 1900
 
     print("Plotting the predictions")
 
@@ -389,8 +389,8 @@ def main(tetrode_number=TETRODE_NUMBER):
 
         plt.savefig('../position/argmax/Position_{}.png'.format(i), bbox_inches='tight')
         plt.close()
-        makeVideo(prediction,actual,points_from,i)
-        plt.close()
+        # makeVideo(prediction,actual,points_from,i)
+        # plt.close()
 
 def makeVideo(prediction, actual, points_from, number):
 
