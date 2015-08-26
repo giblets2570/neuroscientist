b8e59eef93bd0b7d87e4a82a9d6207ecb78c5d6b
giblets2570 <tomkeohanemurray@gmail.com>
2015-08-26 19:53:08 +0100

    yay

--- recurrent_input_labels.py
+++ recurrent_input_labels.py
@@ -249,18 +249,18 @@ def funcs(dataset, network, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, 
 
     # this is the cost of the network when fed throught the noisey network
     train_output = lasagne.layers.get_output(network, X_batch)
-    cost = lasagne.objectives.mse(train_output, y_batch)
+    cost = lasagne.objectives.squared_error(train_output, y_batch)
     cost = cost.mean()
 
     # validation cost
     valid_output = lasagne.layers.get_output(network, X_batch, deterministic=True)
-    valid_cost = lasagne.objectives.mse(valid_output, y_batch)
+    valid_cost = lasagne.objectives.squared_error(valid_output, y_batch)
     valid_cost = valid_cost.mean()
 
     # test the performance of the netowork without noise
     test = lasagne.layers.get_output(network, X_batch, deterministic=True)
     pred = T.argmax(test, axis=1)
-    accuracy = T.mean(lasagne.objectives.mse(pred, y_batch), dtype=theano.config.floatX)
+    accuracy = T.mean(lasagne.objectives.squared_error(pred, y_batch), dtype=theano.config.floatX)
 
     all_params = lasagne.layers.get_all_params(network)
     updates = lasagne.updates.adagrad(cost, all_params, l_rate)
